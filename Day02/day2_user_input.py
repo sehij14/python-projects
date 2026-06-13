41fcb96546bdf1489284a3995c8f669f82bbf68c
sehij14 <sehijpreet14@gmail.com>
2026-06-13 14:09:47 +0530

    Day02: Taking input from user

--- Day02/day2_user_input.py
+++ Day02/day2_user_input.py
@@ -1,6 +1,9 @@
+# Taking input from user
+
 name = input("Enter your name:")
-age = int(input("Enter ypur age:"))
+age = int(input("Enter your age:"))
 city = input("Enter your city")
+
 print("---USER INFORMATION---")
 print("Name:", name)
 print("Age:", age)
