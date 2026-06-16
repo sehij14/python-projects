7161f2e23ab9926b02ddff4a900261e4a59a1918
sehij14 <sehijpreet14@gmail.com>
2026-06-16 14:28:51 +0530

    Day14: Inheritance in Python OOP

--- Day14/day14_inheritance.py
+++ Day14/day14_inheritance.py
@@ -11,7 +11,7 @@ class Employee:
 
     def display_employee(self):
 
-        print("\n----- EMPLOYEE INFO -----")
+        print("\n------EMPLOYEE INFO------")
         print("Name :", self.name)
         print("ID :", self.employee_id)
         print("Salary :", self.salary)
