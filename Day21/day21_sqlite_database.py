13a66bc8788428db1318dc72862032766a13374b
sehij14 <sehijpreet14@gmail.com>
2026-06-17 11:28:04 +0530

    Day21: Introduction to SQLite Databases

---
+++ Day21/day21_sqlite_database.py
@@ -0,0 +1,70 @@
+import sqlite3
+
+# Create Database Connection
+
+connection = sqlite3.connect("school.db")
+
+cursor = connection.cursor()
+
+# Create Table
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS students(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    marks INTEGER
+)
+""")
+
+# Insert Records
+
+cursor.execute(
+"""
+INSERT INTO students(name, age, marks)
+VALUES (?, ?, ?)
+""",
+("Rahul", 18, 90)
+)
+
+cursor.execute(
+"""
+INSERT INTO students(name, age, marks)
+VALUES (?, ?, ?)
+""",
+("Aman", 19, 85)
+)
+
+cursor.execute(
+"""
+INSERT INTO students(name, age, marks)
+VALUES (?, ?, ?)
+""",
+("Priya", 18, 95)
+)
+
+# Save Changes
+
+connection.commit()
+
+# Read Data
+
+cursor.execute("SELECT * FROM students")
+
+students = cursor.fetchall()
+
+print("----- STUDENT DATABASE -----")
+
+for student in students:
+
+    print("\nID:", student[0])
+
+    print("Name:", student[1])
+
+    print("Age:", student[2])
+
+    print("Marks:", student[3])
+
+# Close Connection
+
+connection.close()
