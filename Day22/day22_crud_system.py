2813985c4023784fb1b9685fe5680260d030048f
sehij14 <sehijpreet14@gmail.com>
2026-06-17 14:13:16 +0530

    Day22: CRUD Operations with SQLite

---
+++ Day22/day22_crud_system.py
@@ -0,0 +1,85 @@
+import sqlite3
+
+connection = sqlite3.connect("school.db")
+
+cursor = connection.cursor()
+
+# Create Table
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS students(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    marks INTEGER
+)
+""")
+
+# CREATE
+
+cursor.execute(
+"""
+INSERT INTO students(name, age, marks)
+VALUES (?, ?, ?)
+""",
+("Rahul", 18, 90)
+)
+
+cursor.execute(
+"""
+INSERT INTO students(name, age, marks)
+VALUES (?, ?, ?)
+""",
+("Aman", 19, 85)
+)
+
+connection.commit()
+
+# UPDATE
+
+cursor.execute(
+"""
+UPDATE students
+SET marks = ?
+WHERE name = ?
+""",
+(95, "Rahul")
+)
+
+connection.commit()
+
+# DELETE
+
+cursor.execute(
+"""
+DELETE FROM students
+WHERE name = ?
+""",
+("Aman",)
+)
+
+connection.commit()
+
+# READ
+
+cursor.execute(
+"""
+SELECT * FROM students
+"""
+)
+
+students = cursor.fetchall()
+
+print("----- STUDENT DATABASE -----")
+
+for student in students:
+
+    print("\nID:", student[0])
+
+    print("Name:", student[1])
+
+    print("Age:", student[2])
+
+    print("Marks:", student[3])
+
+connection.close()
