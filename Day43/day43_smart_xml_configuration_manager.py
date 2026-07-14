1415170ac5d30fd3b667e162a235b5b78f9e2295
sehij14 <sehijpreet14@gmail.com>
2026-07-14 21:40:27 +0530

    Day43: Initialize XML Manager

---
+++ Day43/day43_smart_xml_configuration_manager.py
@@ -0,0 +1,112 @@
+from pathlib import Path
+from datetime import datetime
+from xml.etree import ElementTree as ET
+import shutil
+
+
+BASE_FOLDER = Path("Day43")
+CONFIG_FOLDER = BASE_FOLDER / "configurations"
+BACKUP_FOLDER = BASE_FOLDER / "backups"
+XML_FILE = CONFIG_FOLDER / "app_settings.xml"
+
+
+def create_required_folders():
+    CONFIG_FOLDER.mkdir(parents=True, exist_ok=True)
+    BACKUP_FOLDER.mkdir(parents=True, exist_ok=True)
+
+
+def create_xml_file():
+    if XML_FILE.exists():
+        return
+
+    root = ET.Element("settings")
+
+    tree = ET.ElementTree(root)
+
+    tree.write(
+        XML_FILE,
+        encoding="utf-8",
+        xml_declaration=True
+    )
+
+
+def load_tree():
+    return ET.parse(XML_FILE)
+
+
+def save_tree(tree):
+    tree.write(
+        XML_FILE,
+        encoding="utf-8",
+        xml_declaration=True
+    )
+
+
+def backup_xml():
+    if not XML_FILE.exists():
+        return
+
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+    backup_name = f"app_settings_backup_{timestamp}.xml"
+
+    destination = BACKUP_FOLDER / backup_name
+
+    shutil.copy2(XML_FILE, destination)
+
+
+def setting_exists(root, key):
+    for setting in root.findall("setting"):
+        if setting.get("key") == key:
+            return True
+
+    return False
+
+
+def add_setting():
+    tree = load_tree()
+    root = tree.getroot()
+
+    key = input("Setting key: ").strip()
+
+    if not key:
+        print("Key cannot be empty.\n")
+        return
+
+    if setting_exists(root, key):
+        print("Setting already exists.\n")
+        return
+
+    value = input("Setting value: ").strip()
+
+    setting = ET.SubElement(root, "setting")
+
+    setting.set("key", key)
+
+    setting.text = value
+
+    save_tree(tree)
+
+    print("Setting added successfully.\n")
+
+
+def list_settings():
+    tree = load_tree()
+    root = tree.getroot()
+
+    settings = root.findall("setting")
+
+    if not settings:
+        print("No settings found.\n")
+        return
+
+    print("\nApplication Settings\n")
+
+    for index, setting in enumerate(settings, start=1):
+        print(
+            f"{index}. "
+            f"{setting.get('key')} "
+            f"= {setting.text}"
+        )
+
+    print()
